dbf609ad8adbcccf3142e3adbcf4aedab95e2728
Stefan <stefanseifeddine@gmail.com>
2023-04-07 21:43:29 +0100

    Add components and comment out old code

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,86 +1,71 @@
 import React from "react";
-import Overview from "./components/Overview";
-import uniqid from "uniqid";
+import General from "./components/General";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+// import uniqid from "uniqid";
 
-class App extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            task: { text: "", id: uniqid() },
-            tasks: [],
-        };
-    }
-
-    handleChange = (e) => {
-        this.setState({
-            task: {
-                text: e.target.value,
-                id: this.state.task.id,
-            },
-        });
-    };
-
-    onSubmitTask = (e) => {
-        e.preventDefault();
-        this.setState({
-            tasks: this.state.tasks.concat(this.state.task),
-            task: { text: "", id: uniqid() },
-        });
-    };
-
+export default class App extends React.Component {
     render() {
-        const { task, tasks } = this.state;
-
         return (
-            <div className="App">
-                <form onSubmit={this.onSubmitTask}>
-                    <label>Enter task</label>
-                    <input
-                        onChange={this.handleChange}
-                        value={task.text}
-                        type="text"
-                        id="taskInput"
-                    />
-                    <button type="submit">Add task</button>
+            <div>
+                <form>
+                    <General />
+
+                    <Education />
+
+                    <Experience />
                 </form>
-                <Overview tasks={tasks} />
             </div>
         );
     }
 }
 
-export default App;
+// class App extends React.Component {
+//     constructor() {
+//         super();
 
-//
-//
-//
-//
-//
-// import logo from "./logo.svg";
-// import "./App.css";
+//         this.state = {
+//             task: { text: "", id: uniqid() },
+//             tasks: [],
+//         };
+//     }
 
-// {
-//     /* <header className="App-header"> */
+//     handleChange = (e) => {
+//         this.setState({
+//             task: {
+//                 text: e.target.value,
+//                 id: this.state.task.id,
+//             },
+//         });
+//     };
+
+//     onSubmitTask = (e) => {
+//         e.preventDefault();
+//         this.setState({
+//             tasks: this.state.tasks.concat(this.state.task),
+//             task: { text: "", id: uniqid() },
+//         });
+//     };
+
+//     render() {
+//         const { task, tasks } = this.state;
+
+//         return (
+//             <div className="App">
+//                 <form onSubmit={this.onSubmitTask}>
+//                     <label>Enter task</label>
+//                     <input
+//                         onChange={this.handleChange}
+//                         value={task.text}
+//                         type="text"
+//                         id="taskInput"
+//                     />
+//                     <button type="submit">Add task</button>
+//                 </form>
+//                 <Overview tasks={tasks} />
+//             </div>
+//         );
+//     }
 // }
-// {
-//     /* <img src={logo} className="App-logo" alt="logo" /> */
-// }
-// {
-//     /* <p>
-//     Edit <code>src/App.js</code> and save to reload.
-// </p> */
-// }
-// {
-//     /* <a
-//     className="App-link"
-//     href="https://reactjs.org"
-//     target="_blank"
-//     rel="noopener noreferrer"
-// >
-//     Learn React
-// </a> */
-// }
-// {
-//     /* </header> */
-// }
+
+// export default App;
