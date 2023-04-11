5a2292e7842b54a84058bd680566de0845b7615d
Stefan <stefanseifeddine@gmail.com>
2023-04-11 01:49:23 +0100

    Add header component

---
+++ cv-project/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Header.css";
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <h1>CV Application</h1>
+            </header>
+        );
+    }
+}
